bcd7bfc2011f4f7c404ae4929209c75e6dcb6d9a
Davide Cantoni <davide@propertyfinder.ae>
2015-11-03 10:46:33 +0400

    Few small merge fixes

--- webpack/prod.config.js
+++ webpack/prod.config.js
@@ -37,6 +37,7 @@ module.exports = {
       { test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=application/font-woff" },
       { test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=application/octet-stream" },
       { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: "file" },
+      { test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: "file" },
       { test: webpackIsomorphicToolsPlugin.regular_expression('images'), loader: 'url-loader?limit=10240' }
     ]
   },
